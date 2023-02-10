625eac3e4d3f81b614e2e391c24bedbc46ad759f
Jesse Kelly <kellyjesse881@gmail.com>
2023-02-10 02:44:12 +0100

    refactor: refactored into type maps

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,103 @@
-/** @since 0.0.1 */
 
-import { pipe } from 'fp-ts/function'
-
-// -----------------------------------------------------------------------------
-// greetings
-// -----------------------------------------------------------------------------
-
-/**
- * It's a greeting
- *
- * @since 0.0.1
- * @category Greetings
- * @example
- *   import { greet } from 'ast'
- *   assert.deepStrictEqual(greet('World'), 'Hello, World!')
+/*
+ * ast0 - ast types with no arguments
  */
-export const greet = (name: string): string =>
-  pipe(`Hello`, (x) => `${x}, ${name}!`)
+
+export interface NumberKeyword { readonly _tag: "NumberKeyword" }
+export interface BooleanKeyword { readonly _tag: "BooleanKeyword" }
+export interface StringKeyword { readonly _tag: "StringKeyword" }
+export interface BigintKeyword { readonly _tag: "BigintKeyword" }
+export interface NeverKeyword { readonly _tag: "NeverKeyword" }
+
+export interface AST0Map {
+  "NumberKeyword": NumberKeyword;
+  "StringKeyword": StringKeyword;
+  "BooleanKeyword": BooleanKeyword;
+  "BigintKeyword": BigintKeyword;
+  "NeverKeyword": NeverKeyword;
+}
+
+export type AST0 = AST0Map[keyof AST0Map]
+
+export interface AST0TypeMap {
+  "NumberKeyword": number;
+  "StringKeyword": string;
+  "BooleanKeyword": boolean;
+  "BigintKeyword": bigint;
+  "NeverKeyword": never;
+}
+
+/*
+ * ast1 - ast types with one argument
+ */
+
+export interface ArrayKeyword<A1 extends AST> {
+  readonly _tag: "ArrayKeyword"
+  readonly optional: A1
+}
+
+export interface OptionalKeyword<A extends AST> {
+  readonly _tag: "OptionalKeyword"
+  readonly optional: A
+}
+
+interface AST1Map<A extends AST> { 
+  "ArrayKeyword": ArrayKeyword<A>;
+  "OptionalKeyword": OptionalKeyword<A>;
+}
+
+export type AST1<A extends AST> = AST1Map<A>[keyof AST1Map<A>]
+
+export interface AST1TypeMap<A extends AST> {
+  "ArrayKeyword": A[];
+  "OptionalKeyword": A | undefined;
+}
+
+/*
+ * ast2 - ast types with two arguments
+ */
+
+export interface FunctionKeyword<A1 extends AST, A2 extends AST> {
+  readonly _tag: "FunctionKeyword"
+  readonly input: A1
+  readonly output: A2
+}
+
+interface AST2Map<A1 extends AST, A2 extends AST> { 
+  "FunctionKeyword": FunctionKeyword<A1, A2>;
+}
+
+export type AST2<A1 extends AST, A2 extends AST> = AST2Map<A1, A2>[keyof AST2Map<A1, A2>]
+
+export interface AST2TypeMap<A extends AST> {
+  "ArrayKeyword": A[];
+  "OptionalKeyword": A | undefined;
+}
+
+/*
+ * astN - ast types with N arguments (i.e. a tuple of ASTs)
+ */
+
+export interface TupleKeyword<A extends ASTArray> {
+  readonly _tag: "TupleKeyword"
+  readonly tuple: A
+}
+
+interface ASTNMap<A extends ReadonlyArray<AST>> { 
+  "TupleKeyword": TupleKeyword<A>;
+}
+
+/*
+ * ast - the union of all AST types
+*/
+
+export type ASTN<A extends ASTArray> = ASTNMap<A>[keyof ASTNMap<A>]
+
+export type AST = AST0 
+  | ASTArray
+  | AST1<AST0 | ASTArray> 
+  | AST1<AST1<AST0 | ASTArray>> 
+  | AST1<AST1<AST1<AST0 | ASTArray>>>
+  | AST1<AST1<AST1<AST1<AST0 | ASTArray>>>>
+
+export type ASTArray = ReadonlyArray<AST>
